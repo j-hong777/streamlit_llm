678f3863191b77ecccf223f252f81c14e3e5a6d5
eric <ctoadp777@gmail.com>
2025-08-14 23:16:02 +0900

    'streamlit'

---
+++ app.py
@@ -0,0 +1,171 @@
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+import streamlit as st
+from langchain.memory import ConversationSummaryBufferMemory
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    #추후에 텍스트로 천천히 채워나갈 박스임
+    message = ""
+    
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+    
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+    #llm이 생성한 token = token
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+        
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler()
+    ]
+)
+
+
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=600,
+    return_messages=True,
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful AI talking to a human"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ]
+)
+chain = RunnablePassthrough.assign(history=load_memory) | prompt | llm
+
+def load_memory(_):
+    return memory.load_memory_variables({})["history"]
+
+
+def invoke_chain(question):
+    result = chain.invoke({"question": question})
+    memory.save_context(
+        {"input": question},
+        {"output": result.content},
+    )
+    print(result)
+
+
+
+
+
+
+#데코레이터를 통해서 
+#if file:
+#retriever = embed_file(file)
+#을 다시 실행하지 않음
+# 즉 새로운 파일을 업로드하지 않은 경우엔 기존 값을 다시 보여줌
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"        
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+
+st.title("DocumentGPT")
+
+st.markdown("""
+Welcome!
+
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
+
+with st.sidebar:
+    file=st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"])
+
+
+if file:
+    retriever = embed_file(file)
+    send_message("I`m ready!! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file..")
+    
+    if message:
+        send_message(message, "human")
+        #chain 만들기
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm
+
+        #ai가 업데이트 한것처럼 보이게 함
+        with st.chat_message("ai"):        
+            chain.invoke(message)
+      
+else:
+    st.session_state["messages"] = []
